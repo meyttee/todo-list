565ee552f26c8f5a4139ed83045a400be7cba2e6
Mahdi <mahdiprog81@gmail.com>
2025-09-20 07:30:14 +0330

    feat: create layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import "./App.css";
+import { Board, Header, NewColumnModal } from "./components";
+import { StateProvider } from "./providers";
 
 function App() {
-  return <></>;
+  return (
+    <StateProvider>
+      <Header />
+      <Board />
+      <NewColumnModal />
+    </StateProvider>
+  );
 }
 
 export default App;
