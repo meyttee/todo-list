1946d3dcbc785446cfe25449032b1a73451afc29
Mahdi <mahdiprog81@gmail.com>
2025-09-20 22:34:05 +0330

    refactor: remove unnecessary wrapper and prevent inline onClick

--- src/components/ColumnActions/ColumnActions.tsx
+++ src/components/ColumnActions/ColumnActions.tsx
@@ -7,25 +7,21 @@ import { NewTaskModal } from "../NewTaskModal";
 const ColumnAction = ({ id }: { id: string }) => {
   const { dispatch } = useAppState();
 
+  const onClick = () => {
+    dispatch({ type: "DELETE_COLUMN", key: id });
+  };
+
   return (
-    <div>
-      <div className="flex gap-4">
-        <Button
-          variant="icon"
-          onClick={() => {
-            dispatch({ type: "DELETE_COLUMN", key: id });
-          }}
-          title="delete column"
-        >
-          <Icon
-            icon="majesticons:close"
-            width="20"
-            height="20"
-            className="text-[#FF7979]"
-          />
-        </Button>
-        <NewTaskModal id={id} />
-      </div>
+    <div className="flex gap-4">
+      <Button variant="icon" onClick={onClick} title="delete column">
+        <Icon
+          icon="majesticons:close"
+          width="20"
+          height="20"
+          className="text-red-300"
+        />
+      </Button>
+      <NewTaskModal id={id} />
     </div>
   );
 };
