fe508a619c896cf23b85b5c1c07bc05ac7f72e9c
Mahdi <mahdiprog81@gmail.com>
2025-09-20 22:36:50 +0330

    refactor: change colors

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,10 +9,10 @@ const Header = () => {
   }).format(date);
 
   return (
-    <header className="flex items-center justify-between bg-white px-8 py-7 dark:bg-[#2a2b2f]">
+    <header className="flex items-center justify-between bg-white px-8 py-7 dark:bg-neutral-800">
       <h1 className="text-xl font-bold dark:text-white">Todo List</h1>
       <div className="flex items-center gap-2">
-        <h2 className="font-semibold text-[#1C1D22]/50 dark:text-white/50">
+        <h2 className="font-semibold text-neutral-500 dark:text-white/50">
           {today}
         </h2>
         <ThemeSwitch />
