4c5f3a9f4ecf2a4edecef63c352d4486ef08677d
Mahdi <mahdiprog81@gmail.com>
2025-09-20 22:21:52 +0330

    refactor: use default tailwind colors

--- src/components/Button/classes.ts
+++ src/components/Button/classes.ts
@@ -1,8 +1,8 @@
 const baseClasses =
-  "cursor-pointer rounded-3xl bg-[#1C1D22] hover:bg-[#1C1D22]/90 px-6 py-3 text-white transition-colors duration-300 disabled:cursor-default disabled:bg-gray-700 disabled:hover:bg-gray-700 dark:bg-[#4B69FF] dark:hover:bg-[#415bdd]";
+  "cursor-pointer rounded-3xl bg-neutral-900 hover:bg-neutral-800 px-6 py-3 text-white transition-colors duration-300 disabled:cursor-default disabled:bg-gray-700 disabled:hover:bg-gray-700 dark:bg-blue-700 dark:hover:bg-blue-600";
 
 const iconButtonClasses =
-  "cursor-pointer rounded-full p-2 hover:bg-[#1C1D22]/10 dark:hover:bg-white/10";
+  "cursor-pointer rounded-full p-2 hover:bg-neutral-900/10 dark:hover:bg-white/10";
 
 const fabButtonClasses = "fixed bottom-5 left-5 size-19 rounded-full";
 
