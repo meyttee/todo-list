b7c3b6da34bfb2f13d7777b9e91ae1e0abcdfb4e
Mahdi <mahdiprog81@gmail.com>
2025-09-20 07:27:03 +0330

    feat: create switch for change theme

---
+++ src/components/ThemeSwitch/ThemeSwitch.tsx
@@ -0,0 +1,15 @@
+import { Switch } from "../switch";
+import { useChangeTheme } from "../../hooks";
+
+const ThemeSwitch = () => {
+  const { changeThemeHandler, theme } = useChangeTheme();
+  return (
+    <Switch
+      checked={theme === "light"}
+      onChange={changeThemeHandler}
+      variant="theme"
+    />
+  );
+};
+
+export default ThemeSwitch;
