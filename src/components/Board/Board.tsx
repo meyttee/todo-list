ac4fede986a600d0c129d60986bb8e90bf061e30
Mahdi <mahdiprog81@gmail.com>
2025-09-20 07:29:56 +0330

    feat: assemble board

---
+++ src/components/Board/Board.tsx
@@ -0,0 +1,17 @@
+import { Column } from "../Column";
+import { Container } from "../Container";
+import { useAppState } from "../../hooks";
+
+const Board = () => {
+  const { state } = useAppState();
+
+  return (
+    <Container>
+      {Array.from(state.data.keys()).map((key) => (
+        <Column key={key} id={key} />
+      ))}
+    </Container>
+  );
+};
+
+export default Board;
