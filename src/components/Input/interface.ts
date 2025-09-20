a8096c1ea0ff97067db95cfa7b2920741cf0b6d8
Mahdi <mahdiprog81@gmail.com>
2025-09-20 22:41:27 +0330

    refactor: change colors and move types to interface file

--- src/components/Input/interface.ts
+++ src/components/Input/interface.ts
@@ -1,15 +1,21 @@
 import type { DetailedHTMLProps, InputHTMLAttributes } from "react";
 import type { Control, FieldValues, Path } from "react-hook-form";
 
-interface IControlledProps<TFieldValues extends FieldValues>
-  extends Omit<
-    DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>,
-    "name"
+interface IInputProps
+  extends DetailedHTMLProps<
+    InputHTMLAttributes<HTMLInputElement>,
+    HTMLInputElement
   > {
+  error?: string;
+  label?: string;
+}
+
+interface IControlledProps<TFieldValues extends FieldValues>
+  extends Omit<IInputProps, "name"> {
   control?: Control<TFieldValues>;
   name: Path<TFieldValues>;
   description?: string;
   label?: string;
 }
 
-export type { IControlledProps };
+export type { IControlledProps, IInputProps };
