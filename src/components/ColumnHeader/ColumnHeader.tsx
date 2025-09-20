38b9546ec48f5c290c9a2a2ade7eb3d91e473f77
Mahdi <mahdiprog81@gmail.com>
2025-09-20 22:34:33 +0330

    style: remove return

--- src/components/ColumnHeader/ColumnHeader.tsx
+++ src/components/ColumnHeader/ColumnHeader.tsx
@@ -1,13 +1,11 @@
 import { ColumnTitle } from "../ColumnTitle";
 import { ColumnAction } from "../ColumnActions";
 
-const ColumnHeader = ({ title }: { title: string }) => {
-  return (
-    <div className="flex w-full items-center justify-between">
-      <ColumnTitle title={title} />
-      <ColumnAction id={title} />
-    </div>
-  );
-};
+const ColumnHeader = ({ title }: { title: string }) => (
+  <div className="flex w-full items-center justify-between">
+    <ColumnTitle title={title} />
+    <ColumnAction id={title} />
+  </div>
+);
 
 export default ColumnHeader;
