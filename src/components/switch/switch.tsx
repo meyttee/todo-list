99d4e31668a7913805708fb420cabcc43147097c
Mahdi <mahdiprog81@gmail.com>
2025-09-20 07:26:47 +0330

    feat: create switch

---
+++ src/components/switch/switch.tsx
@@ -0,0 +1,39 @@
+import clsx from "clsx";
+
+const Switch = ({
+  checked,
+  label,
+  onChange,
+  variant,
+}: {
+  checked?: boolean;
+  onChange?: () => void;
+  label?: string;
+  variant: "theme" | "normal";
+}) => {
+  return (
+    <label className="inline-flex cursor-pointer items-center">
+      <input
+        type="checkbox"
+        value=""
+        className="peer sr-only"
+        checked={checked}
+        onChange={onChange}
+      />
+      <div
+        className={clsx(
+          `peer relative h-6 w-11 rounded-full bg-gray-200 peer-checked:bg-blue-600 peer-focus:outline-none after:absolute after:start-[2px] after:top-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:after:translate-x-full peer-checked:after:border-white rtl:peer-checked:after:-translate-x-full dark:border-gray-600 dark:bg-gray-700 dark:peer-checked:bg-blue-600`,
+          {
+            [`h-7 after:border-none after:!bg-transparent after:content-['🌜'] peer-checked:after:content-['🌞']`]:
+              variant === "theme",
+          },
+        )}
+      />
+      <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+        {label}
+      </span>
+    </label>
+  );
+};
+
+export default Switch;
