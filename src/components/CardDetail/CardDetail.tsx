9a680866c95615eb06efdfb1e8cd96202c9e33e4
Mahdi <mahdiprog81@gmail.com>
2025-09-20 22:29:58 +0330

    refactor: prevent inline functions and repeated classes

--- src/components/CardDetail/CardDetail.tsx
+++ src/components/CardDetail/CardDetail.tsx
@@ -1,48 +1,48 @@
-import { useAppState } from "../../hooks";
-import { Button } from "../Button";
 import { Modal } from "../Modal";
+import { Button } from "../Button";
+import { useAppState } from "../../hooks";
 
-const CardDetailModal = ({
-  isOpen,
-  onClose,
-  description,
-  status,
-  title,
-  id,
-}: {
-  onClose: () => void;
-  isOpen: boolean;
-  title: string;
-  description: string;
-  status: "done" | "todo";
-  id: string;
-}) => {
+import type { ICardDetailProps } from "./interface";
+
+const CardDetailModal = (props: ICardDetailProps) => {
+  const { isOpen, onClose, description, status, title, id, columnId } = props;
+
   const { dispatch } = useAppState();
+
+  const sectionBodyClasses = "text-lg dark:text-white/80";
+  const titleClasses = "border-b border-gray-600 text-xl dark:text-white";
+
+  const clickHandler = () => {
+    const preparedData = {
+      columnId,
+      taskId: id,
+    };
+
+    if (status === "done") {
+      dispatch({
+        type: "MARK_AS_TODO",
+        ...preparedData,
+      });
+    } else {
+      dispatch({
+        type: "MARK_AS_DONE",
+        ...preparedData,
+      });
+    }
+  };
+
   return (
     <Modal onClose={onClose} isOpen={isOpen} title="Detail">
       <div className="flex flex-col gap-5">
-        <h2 className="border-b border-gray-600 text-xl dark:text-white">
-          Title
-        </h2>
-        <h3
-          onClick={() =>
-            dispatch({ type: "MARK_AS_DONE", colId: "col 1", taskId: id })
-          }
-          className="text-lg dark:text-white/80"
-        >
-          {title}
-        </h3>
-        <h2 className="border-b border-gray-600 text-xl dark:text-white">
-          Description
-        </h2>
-        <h5 className="text-lg dark:text-white/80">{description}</h5>
-        <h2 className="border-b border-gray-600 text-xl dark:text-white">
-          Status
-        </h2>
-        <h5 className="text-lg dark:text-white/80">{status}</h5>
+        <h2 className={titleClasses}>Title</h2>
+        <h5 className={sectionBodyClasses}>{title}</h5>
+        <h2 className={titleClasses}>Description</h2>
+        <h5 className={sectionBodyClasses}>{description}</h5>
+        <h2 className={titleClasses}>Status</h2>
+        <h5 className={sectionBodyClasses}>{status}</h5>
         <div className="flex justify-end">
-          <Button variant="normal" disabled={status === "done"}>
-            Mark As Done
+          <Button variant="normal" onClick={clickHandler}>
+            {status === "done" ? "Mark As TODO" : "Mark As DONE"}
           </Button>
         </div>
       </div>
