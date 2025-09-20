c7a547b8309c4d5d9b5bca01b5d9c766b4dfad9d
Mahdi <mahdiprog81@gmail.com>
2025-09-20 22:35:01 +0330

    style: remove return

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,11 +1,7 @@
 import type { PropsWithChildren } from "react";
 
-const Container = ({ children }: PropsWithChildren) => {
-  return (
-    <section className="flex gap-5 overflow-x-auto px-8 py-5">
-      {children}
-    </section>
-  );
-};
+const Container = ({ children }: PropsWithChildren) => (
+  <section className="flex gap-5 overflow-x-auto px-8 py-5">{children}</section>
+);
 
 export default Container;
