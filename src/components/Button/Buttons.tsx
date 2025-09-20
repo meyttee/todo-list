02aaf5217efee4373ef90d86d93a7133d72ecd88
Mahdi <mahdiprog81@gmail.com>
2025-09-20 07:22:12 +0330

    feat: create button

---
+++ src/components/Button/Buttons.tsx
@@ -0,0 +1,22 @@
+import clsx from "clsx";
+import type { IBaseButtonProps } from "./interface";
+import { baseClasses, fabButtonClasses, iconButtonClasses } from "./classes";
+
+const Button = ({ variant, ...props }: IBaseButtonProps) => {
+  return (
+    <button
+      {...props}
+      className={clsx(
+        "flex items-center justify-center",
+        {
+          [iconButtonClasses]: variant === "icon",
+          [baseClasses]: variant === "normal" || variant === "fab",
+          [fabButtonClasses]: variant === "fab",
+        },
+        props.className,
+      )}
+    />
+  );
+};
+
+export default Button;
---
+++ src/components/Button/classes.ts
@@ -0,0 +1,9 @@
+const baseClasses =
+  "cursor-pointer rounded-3xl bg-[#1C1D22] hover:bg-[#1C1D22]/90 px-6 py-3 text-white transition-colors duration-300 disabled:cursor-default disabled:bg-gray-700 disabled:hover:bg-gray-700 dark:bg-[#4B69FF] dark:hover:bg-[#415bdd]";
+
+const iconButtonClasses =
+  "cursor-pointer rounded-full p-2 hover:bg-[#1C1D22]/10 dark:hover:bg-white/10";
+
+const fabButtonClasses = "fixed bottom-5 left-5 size-19 rounded-full";
+
+export { baseClasses, iconButtonClasses, fabButtonClasses };
