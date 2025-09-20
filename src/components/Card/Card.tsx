0bbd37410f95c8545423ba8c9a08ffb19e5f621d
Mahdi <mahdiprog81@gmail.com>
2025-09-20 22:24:50 +0330

    style: create prevent inline functions

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,26 +1,21 @@
-import { useState } from "react";
-import { Icon } from "@iconify/react";
 import clsx from "clsx";
-import { useAppState } from "../../hooks";
-import { CardDetailModal } from "../CardDetail";
-import { Button } from "../Button";
+import { Icon } from "@iconify/react";
+import { useState, type MouseEvent } from "react";
 
-const Card = ({
-  description,
-  status,
-  title,
-  id,
-  colId,
-}: {
-  title: string;
-  description: string;
-  status: "done" | "todo";
-  id: string;
-  colId: string;
-}) => {
-  const [showModal, setShowModal] = useState(false);
-  const onClose = () => setShowModal(false);
+import { Button } from "../Button";
+import { useAppState } from "../../hooks";
+import { valuesLabel } from "../../constants";
+import { CardDetailModal } from "../CardDetail";
+
+import type { ICardProps } from "./interface";
+
+const Card = (props: ICardProps) => {
+  const { status, title, id, columnId, priority } = props;
+
   const { dispatch } = useAppState();
+  const [showModal, setShowModal] = useState(false);
+
+  const onClose = () => setShowModal(false);
 
   const handleDragStart = (
     e: React.DragEvent,
@@ -33,43 +28,58 @@ const Card = ({
     );
   };
 
+  const handleOpenModal = (
+    e: MouseEvent<HTMLDivElement, globalThis.MouseEvent>,
+  ) => {
+    e.stopPropagation();
+    setShowModal(true);
+  };
+
+  const handleDelete = () => {
+    dispatch({ type: "DELETE_TASK", columnId, taskId: id });
+  };
+
   return (
     <div
       draggable
-      onDragStart={(e) => handleDragStart(e, colId, id)}
-      className="flex w-full items-center rounded-xl border border-gray-200 bg-white p-5 dark:border-white/10 dark:bg-[#292B31]"
+      onDragStart={(e) => handleDragStart(e, columnId, id)}
+      className="flex w-full items-start rounded-xl border border-gray-200 bg-white p-5 dark:border-white/10 dark:bg-neutral-800"
     >
       <CardDetailModal
-        id={id}
+        {...props}
         isOpen={showModal}
         onClose={onClose}
-        description={description}
-        title={title}
-        status={status}
+        columnId={columnId}
       />
       <div
-        className={clsx("w-full cursor-pointer dark:text-white", {
-          "line-through": status === "done",
-        })}
-        onClick={(e) => {
-          e.stopPropagation();
-          setShowModal(true);
-        }}
+        className={clsx(
+          "flex w-full cursor-pointer flex-col gap-1 dark:text-white",
+          {
+            "line-through": status === "done",
+          },
+        )}
+        onClick={handleOpenModal}
       >
-        {title}
+        <h4>{title}</h4>
+        <h6
+          className={clsx(
+            "flex items-center gap-2 before:block before:size-4 before:rounded-full before:content-['']",
+            {
+              "before:!bg-red-400": priority === 3,
+              "before:!bg-blue-400": priority === 2,
+              "before:!bg-green-400": priority === 1,
+            },
+          )}
+        >
+          {valuesLabel[priority]}
+        </h6>
       </div>
-      <Button
-        variant="icon"
-        onClick={() => {
-          dispatch({ type: "DELETE_TASK", colId, taskId: id });
-        }}
-        title="delete column"
-      >
+      <Button variant="icon" onClick={handleDelete} title="delete column">
         <Icon
           icon="majesticons:close"
           width="20"
           height="20"
-          className="text-[#FF7979]"
+          className="text-red-300"
         />
       </Button>
     </div>
