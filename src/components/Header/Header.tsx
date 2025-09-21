cde9a6dd0cda91d76daad446dadbaa2794873839
Mahdi <mahdiprog81@gmail.com>
2025-09-21 16:58:17 +0330

    fix: header responsive style

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -10,9 +10,11 @@ const Header = () => {
 
   return (
     <header className="flex items-center justify-between bg-white px-8 py-7 dark:bg-neutral-800">
-      <h1 className="text-xl font-bold dark:text-white">Todo List</h1>
+      <h1 className="text-md font-bold sm:text-xl dark:text-white">
+        Todo List
+      </h1>
       <div className="flex items-center gap-2">
-        <h2 className="font-semibold text-neutral-500 dark:text-white/50">
+        <h2 className="text-[10px] font-medium text-neutral-500 sm:text-sm dark:text-white/50">
           {today}
         </h2>
         <ThemeSwitch />
