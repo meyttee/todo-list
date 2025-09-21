d416f609abc617d5620c293ab441cc444f431829
Mahdi <mahdiprog81@gmail.com>
2025-09-21 16:57:54 +0330

    feat: add shadow to switch button

--- src/components/Form/Switch/Switch.tsx
+++ src/components/Form/Switch/Switch.tsx
@@ -20,9 +20,9 @@ const Switch = ({
       />
       <div
         className={clsx(
-          `peer relative h-6 w-11 rounded-full bg-gray-200 peer-checked:bg-blue-600 peer-focus:outline-none after:absolute after:start-[2px] after:top-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:after:translate-x-full peer-checked:after:border-white rtl:peer-checked:after:-translate-x-full dark:border-gray-600 dark:bg-gray-700 dark:peer-checked:bg-blue-600`,
+          `peer inset-shadow-md relative h-6 w-11 rounded-full bg-gray-200 inset-shadow-gray-500 duration-700 peer-checked:bg-blue-600 peer-focus:outline-none after:absolute after:start-[2px] after:top-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:after:translate-x-full peer-checked:after:border-white rtl:peer-checked:after:-translate-x-full dark:border-gray-600 dark:bg-gray-700 dark:peer-checked:bg-blue-600`,
           {
-            [`h-7 after:border-none after:!bg-transparent after:content-['🌜'] peer-checked:after:content-['🌞']`]:
+            [`h-7 duration-700 after:border-none after:!bg-transparent after:content-['🌜'] peer-checked:after:rotate-[360deg] peer-checked:after:content-['🌞']`]:
               variant === "theme",
           },
         )}
