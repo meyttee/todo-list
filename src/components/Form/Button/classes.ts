4d4976f2cd95509b11178bdf7fc1c0658e3d4521
Mahdi <mahdiprog81@gmail.com>
2025-09-21 16:57:31 +0330

    fix: change buttons style

--- src/components/Form/Button/classes.ts
+++ src/components/Form/Button/classes.ts
@@ -2,8 +2,9 @@ const baseClasses =
   "cursor-pointer rounded-3xl bg-neutral-900 hover:bg-neutral-800 px-6 py-3 text-white transition-colors duration-300 disabled:cursor-default disabled:bg-gray-700 disabled:hover:bg-gray-700 dark:bg-blue-700 dark:hover:bg-blue-600";
 
 const iconButtonClasses =
-  "cursor-pointer rounded-full p-2 hover:bg-neutral-900/10 dark:hover:bg-white/10";
+  "cursor-pointer rounded-full p-2 hover:bg-neutral-900/10 dark:hover:bg-white/10 transition duration-300";
 
-const fabButtonClasses = "fixed bottom-5 left-5 size-19 rounded-full";
+const fabButtonClasses =
+  "fixed bottom-5 left-5 size-19 rounded-full shadow-sm shadow-gray-500 hover:scale-105 hover:transition hover:duration-300";
 
 export { baseClasses, iconButtonClasses, fabButtonClasses };
