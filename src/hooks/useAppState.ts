90f8381455bfb3c1e75488afa30cf1299e72ef3a
Mahdi <mahdiprog81@gmail.com>
2025-09-21 22:30:21 +0330

    fix: context error handling

--- src/hooks/useAppState.ts
+++ src/hooks/useAppState.ts
@@ -3,6 +3,9 @@ import { StateContext } from "../contexts";
 
 const useAppState = () => {
   const context = useContext(StateContext);
+  if (!context) {
+    throw new Error("useAppState should inside StateContext");
+  }
   return context;
 };
 
