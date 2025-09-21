fb184e09e86c2722435eb4a0b1d3ad5ec6db1bc2
Mahdi <mahdiprog81@gmail.com>
2025-09-21 22:29:13 +0330

    style: context error remove

--- src/hooks/useAppState.ts
+++ src/hooks/useAppState.ts
@@ -3,9 +3,6 @@ import { StateContext } from "../contexts";
 
 const useAppState = () => {
   const context = useContext(StateContext);
-  if (!context) {
-    throw new Error("useAppState must be used inside a StateProvider");
-  }
   return context;
 };
 
